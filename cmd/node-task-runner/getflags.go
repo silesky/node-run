33bec60d3b6e0258debb1f02bb2c823e8f2bed42
Seth Silesky <5115498+silesky@users.noreply.github.com>
2024-12-28 15:02:44 -0600

    wip

--- cmd/node-task-runner/getflags.go
+++ cmd/node-task-runner/getflags.go
@@ -14,12 +14,15 @@ type flags struct {
 func getFlags() flags {
 	help := flag.Bool("help", false, "Print help")
 	version := flag.Bool("version", false, "Print version")
-	// Custom usage function
+
+	// custom help function
 	flag.Usage = func() {
 		fmt.Fprintf(os.Stderr, "\nUsage of ntk \n")
 		flag.VisitAll(func(f *flag.Flag) {
+			// override to show e.g (--flag) instead of -flag
 			fmt.Fprintf(os.Stderr, "  --%s\n\t%s\n", f.Name, f.Usage)
 		})
+
 	}
 	flag.Parse()
 	return flags{
