491b15b62a24a28fe4084247e6c74a588ae2492a
Seth Silesky <5115498+silesky@users.noreply.github.com>
2024-12-29 18:05:36 -0600

    wip

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -10,19 +10,16 @@ var (
 	debug       = false
 )
 
-// SetDebug sets the debug flag
 func SetDebug(d bool) {
 	debug = d
 }
 
-// Debug logs an info message if debugging is enabled
 func Debug(v ...interface{}) {
 	if debug {
 		DebugLogger.Println(v...)
 	}
 }
 
-// Debugf logs a formatted info message if debugging is enabled
 func Debugf(format string, v ...interface{}) {
 	if debug {
 		DebugLogger.Printf(format, v...)
