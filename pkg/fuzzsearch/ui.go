45cf58e08efc4e050a8b4789b8bbf14a78aef534
Seth Silesky <5115498+silesky@users.noreply.github.com>
2024-12-31 11:31:23 -0600

    wip

--- pkg/fuzzsearch/ui.go
+++ pkg/fuzzsearch/ui.go
@@ -8,42 +8,51 @@ import (
 	"github.com/charmbracelet/lipgloss"
 )
 
-// Model represents the Bubble Tea CommandModel
-type CommandModel struct {
-	commands       []Command
-	filtered       []Command
-	cursor         int
-	input          string
-	quitting       bool
-	highlightStyle lipgloss.Style
-	filteredStyle  lipgloss.Style
+// TeaCommandModel represents the bubbletea Model API
+type TeaCommandModel struct {
+	commands []Command
+	filtered []Command
+	cursor   int
+	input    string
+	quitting bool
+	styles   Styles
 }
 
-func (m *CommandModel) Init() tea.Cmd {
+// Init is required implementation
+func (m *TeaCommandModel) Init() tea.Cmd {
 	return nil
 }
-
-func (m *CommandModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+// Init is required implementation
+func (m *TeaCommandModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 
 	case tea.KeyMsg:
 		switch msg.String() {
+		// return the selected value and quit
 		case "ctrl+c", "q":
 			m.quitting = true
 			return m, tea.Quit
 
+		// return the selected varlua and quit
 		case "enter":
 			return m, tea.Quit
 
+			// move the cursor up
 		case "up":
 			if m.cursor > 0 {
 				m.cursor--
 			}
 
+		// move the cursor down
 		case "down":
 			if m.cursor < len(m.filtered)-1 {
 				m.cursor++
 			}
+
+		case "left"
+		case "right":
+			// do nothing
+
 
 		case "backspace":
 			if len(m.input) > 0 {
@@ -64,11 +73,12 @@ func (m *CommandModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	return m, nil
 }
 
-func (m CommandModel) View() string {
+// View is required implementation for tea
+func (m TeaCommandModel) View() string {
 	var lines strings.Builder
 
 	lines.WriteString("\n Filter: ")
-	lines.WriteString(m.filteredStyle.Render(m.input))
+	lines.WriteString(m.styles.filterInput.Render(m.input))
 	lines.WriteString("\n\n")
 
 	for i, cmd := range m.filtered {
@@ -81,7 +91,7 @@ func (m CommandModel) View() string {
 		line := fmt.Sprintf("%s [%s] %s - %s", cursor, cmd.PackageName, cmd.Name, cmd.Command)
 		line = strings.Replace(line, cursor, "", 1)
 		if m.cursor == i {
-			lines.WriteString(cursor + m.highlightStyle.Render(line))
+			lines.WriteString(cursor + m.styles.selected.Render(line))
 		} else {
 			lines.WriteString(cursor + line)
 		}
@@ -111,13 +121,25 @@ func filterCommands(commands []Command, query string) []Command {
 	return result
 }
 
+type Styles struct {
+	selected    lipgloss.Style
+	filterInput lipgloss.Style
+}
+
+func newStyles() Styles {
+	hotPink := lipgloss.Color("205")
+	return Styles{
+		selected:    lipgloss.NewStyle().Foreground(hotPink),
+		filterInput: lipgloss.NewStyle().Foreground(hotPink),
+	}
+}
+
 // commandSelector displays the command selector UI
 func DisplayCommandSelector(commands []Command) (*Command, error) {
-	m := &CommandModel{
-		commands:       commands,
-		filtered:       commands, // Start with all commands
-		highlightStyle: lipgloss.NewStyle().Foreground(lipgloss.Color("205")),
-		filteredStyle:  lipgloss.NewStyle().Foreground(lipgloss.Color("205")),
+	m := &TeaCommandModel{
+		commands: commands,
+		filtered: commands, // Start with all commands
+		styles:   newStyles(),
 	}
 
 	program := tea.NewProgram(m)
